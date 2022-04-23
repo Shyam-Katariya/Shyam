bee0f5e8576a95a86a752b2a3c3599d4a6e258fc
Shyam-Katariya <shyamkatariya96@gmail.com>
2022-04-23 14:36:44 +0530

    First Commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class student {
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i = 12;
+        int l = 22;
+        int sum;
+
+        System.out.println("Number 1 = " + i);
+        System.out.println("Number 2 = " + l);
+
+        sum = i + l;
+        System.out.println("Total Sum of two numbers = " + sum);
+    }
+
+}
